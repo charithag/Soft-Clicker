f6483eb7de09eb2471fab56bf3a8a4dda54eaa0f
OSMiUM <OSMiUM>
2016-03-22 15:12:22 +0530

    updated ui, APN selection in app

--- android-new/app/src/main/java/org/softclicker/android/soft_clicker/ListnerActivity.java
+++ android-new/app/src/main/java/org/softclicker/android/soft_clicker/ListnerActivity.java
@@ -1,16 +1,22 @@
 package org.softclicker.android.soft_clicker;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by User on 3/18/2016.
  */
 public class ListnerActivity extends AppCompatActivity {
+    public static final String SSID = "ssid";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listner);
+        Bundle extras = getIntent().getExtras();
+        String selectedSSID = extras.getString(SSID);
+        TextView statusLabel = (TextView) findViewById(R.id.class_status_label);
+        statusLabel.setText("Your are connected to " + selectedSSID);
     }
 }
--- android-new/app/src/main/java/org/softclicker/android/soft_clicker/MainActivity.java
+++ android-new/app/src/main/java/org/softclicker/android/soft_clicker/MainActivity.java
@@ -1,18 +1,26 @@
 package org.softclicker.android.soft_clicker;
 
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 
 import org.softclicker.android.soft_clicker.apn.ApnAdapter;
@@ -23,32 +31,78 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     WifiManager mainWifiObj;
-    private ArrayList<ApnData> apnArray;
     private ApnAdapter apnAdapter;
+    private ListView listView;
+    private ProgressBar loader;
+    private List<ScanResult> wifiScanList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // initialize progressBar
+        loader = (ProgressBar) findViewById(R.id.progressBar);
+        loader.setVisibility(View.VISIBLE);
 
-        /** Load student information from the localstorage */
+        listView = (ListView) findViewById(R.id.apnListView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final ScanResult selectedSSID = wifiScanList.get(position);
+                final String[] apnPw = {""};
+
+                LayoutInflater li = LayoutInflater.from(MainActivity.this);
+                View promptsView = li.inflate(R.layout.prompt_apn_pw, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        MainActivity.this);
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView.findViewById(R.id.apnPwPromptText);
+
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        // result.setText(userInput.getText());
+                                        apnPw[0] = userInput.getText().toString();
+                                        // TODO connect with APN
+
+                                        // after connection is successful
+                                        // WifiInfo connectionInfo = mainWifiObj.getConnectionInfo();
+                                        Intent i = new Intent(MainActivity.this, ListnerActivity.class);
+                                        i.putExtra(ListnerActivity.SSID, selectedSSID.SSID);
+
+                                        startActivity(i);
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        });
+
+        /** TODO : Load student information from the localstorage */
+        // Temporarily bind dummy data
         Spinner studentSelector = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.student_list, android.R.layout.simple_spinner_item);
-
-        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // Apply the adapter to the spinner
         studentSelector.setAdapter(adapter);
 
         /** Load APNs */
         mainWifiObj = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-
+        // Switch on WiFi
         if (!mainWifiObj.isWifiEnabled()) {
             mainWifiObj.setWifiEnabled(true);
         }
-
         MyWifiReceiver broadcastReceiver = new MyWifiReceiver(this);
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
@@ -70,7 +124,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()) {
             case R.id.refresh_menu:
                 mainWifiObj.startScan();
@@ -80,24 +133,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void navigateToListner(View view) {
-        Intent i = new Intent(this, ListnerActivity.class);
-        startActivity(i);
-    }
-
+    // This methods binds WiFi APN data to the ListView
     public void setData() {
+        loader.setVisibility(View.VISIBLE);
+
         WifiScanReceiver wifiReceiver = new WifiScanReceiver();
         registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-
-        List<ScanResult> wifiScanList = mainWifiObj.getScanResults();
-
-        apnArray = new ArrayList<>();
+        wifiScanList = mainWifiObj.getScanResults();
+        ArrayList<ApnData> apnArray = new ArrayList<>();
         for (int i = 0; i < wifiScanList.size(); i++) {
             apnArray.add(new ApnData(wifiScanList.get(i).SSID, wifiScanList.get(i).BSSID));
         }
         apnAdapter = new ApnAdapter(this, apnArray);
-        ListView listView = (ListView) findViewById(R.id.apnListView);
         listView.setAdapter(apnAdapter);
+
+        loader.setVisibility(View.GONE);
     }
 }
 
