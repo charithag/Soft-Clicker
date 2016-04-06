043c5730e8fc8898b57f249ce0c30ad0a92e21f6
Rasika Perera <info.rasika@gmail.com>
2016-04-06 18:29:20 +0530

    Adding user Id to User entity

--- soft-clicker-server/src/main/java/org/softclicker/server/entity/User.java
+++ soft-clicker-server/src/main/java/org/softclicker/server/entity/User.java
@@ -7,14 +7,15 @@ import java.util.List;
  * You can create new {@link User} only by passing a parameters through the constructor.
  */
 public class User {
+    final private int userId;
+    final private String firstName;
+    final private String lastName;
+    final private String userName;
+    final private char[] password;
+    final private List<Role> roles;
 
-    private final String firstName;
-    private final String lastName;
-    private final String userName;
-    private final char[] password;
-    private List<Role> roles;
-
-    public User(String firstName, String lastName, String userName, char[] password, List<Role> roles) {
+    public User(int userId, String firstName, String lastName, String userName, char[] password, List<Role> roles) {
+        this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.userName = userName;
@@ -24,6 +25,10 @@ public class User {
 
     public String getFullName() {
         return firstName + " " + lastName;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 
     public String getFirstName() {
@@ -38,12 +43,12 @@ public class User {
         return userName;
     }
 
-    public List<Role> getRoles() {
-        return roles;
-    }
-
     public char[] getPassword() {
         return password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
     }
 
     public enum Role {
