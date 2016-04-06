6aba22d066d118b023ab4e19c99b7677b1f1b9bd
Rasika Perera <info.rasika@gmail.com>
2016-04-06 23:25:28 +0530

    Printing dummy data

--- soft-clicker-server/src/main/java/org/softclicker/server/starup/Bootstrap.java
+++ soft-clicker-server/src/main/java/org/softclicker/server/starup/Bootstrap.java
@@ -4,7 +4,11 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.softclicker.server.config.ServerConfigManager;
 import org.softclicker.server.dao.ScopingDataSource;
+import org.softclicker.server.entity.Answer;
+import org.softclicker.server.entity.Question;
 import org.softclicker.server.entity.User;
+import org.softclicker.server.manage.AnswerManager;
+import org.softclicker.server.manage.QuestionManager;
 import org.softclicker.server.manage.UserManager;
 
 public class Bootstrap {
@@ -15,9 +19,23 @@ public class Bootstrap {
         ScopingDataSource scopingDataSource = new ScopingDataSource(new ServerConfigManager());
         DatabaseCreator creator = new DatabaseCreator(scopingDataSource);
         creator.createDbStructureIfNotExists();
+        //read all users
+        log.info("-----------------Users-------------------");
         UserManager userManager = new UserManager(scopingDataSource);
-        for(User user : userManager.getAllUsers()){
+        for (User user : userManager.getAllUsers()) {
             log.info(user);
+        }
+        //read all questions
+        log.info("---------------Questions-----------------");
+        QuestionManager questionManager = new QuestionManager(scopingDataSource);
+        for (Question question : questionManager.getAllQuestions()) {
+            log.info(question);
+        }
+        //read all answers
+        log.info("----------------Answers------------------");
+        AnswerManager answerManager = new AnswerManager(scopingDataSource);
+        for (Answer answer : answerManager.getAllAnswers()) {
+            log.info(answer);
         }
         log.info("SoftClicker Server Started!");
     }
