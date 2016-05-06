def9e6d7bf833e3b9d37b82675900dc7f9ed6082
chamika <chamikaw@gmail.com>
2016-05-06 11:39:12 +0530

    Add history view data binding, class data rerieval

---
+++ soft-clicker-server/src/main/java/org/softclicker/server/entity/Clazz.java
@@ -0,0 +1,40 @@
+package org.softclicker.server.entity;
+
+/**
+ * Created by chamika on 5/6/16.
+ */
+public class Clazz {
+    private int id;
+    private String name;
+    private int year;
+
+    public Clazz(int id, String name, int year) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
