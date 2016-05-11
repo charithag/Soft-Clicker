641f220e5cafaf2a3a7ade3f2ea8749e7034d50c
sajithaPriyankara <asksajitha@gmail.com>
2016-05-11 23:02:07 +0530

    Merge remote-tracking branch 'origin/master' student id passing

--- windows-app/src/main/java/org.softclicker.client/gui/controllers/AnswerPane.java
+++ windows-app/src/main/java/org.softclicker.client/gui/controllers/AnswerPane.java
@@ -4,12 +4,19 @@ package org.softclicker.client.gui.controllers;
  * Created by Admin on 5/6/2016.
  */
 
+import com.jfoenix.controls.JFXDecorator;
 import io.datafx.controller.FXMLController;
+import io.datafx.controller.flow.Flow;
 import io.datafx.controller.flow.FlowException;
+import io.datafx.controller.flow.FlowHandler;
+import io.datafx.controller.flow.container.DefaultFlowContainer;
+import io.datafx.controller.flow.context.FXMLViewFlowContext;
+import io.datafx.controller.flow.context.ViewFlowContext;
 import io.datafx.controller.util.VetoException;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -64,16 +71,49 @@ public class AnswerPane {
     private String studentId;
     private SoftClickAnswer.AnswerOption answerOption;
 
+    private FlowHandler flowHandler;
+    @FXMLViewFlowContext
+    private ViewFlowContext context;
+
     @PostConstruct
     public void init() throws FlowException, VetoException {
 
         Stage stage = MainWindow.primaryStage;
-        idoOutputLable.setText(this.studentId);
+        String sid = (String)context.getRegisteredObject("sid");
+        idoOutputLable.setText(sid);
         final ToggleGroup group = new ToggleGroup();
         radioButton1.setToggleGroup(group);
         radioButton2.setToggleGroup(group);
         radioButton3.setToggleGroup(group);
         radioButton4.setToggleGroup(group);
+
+        changeIdButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+
+                try {
+
+                    Stage stage = new Stage();
+                    Flow flow = new Flow(MainController.class);
+                    DefaultFlowContainer container = new DefaultFlowContainer();
+                    context = new ViewFlowContext();
+                    context.register("Stage", stage);
+
+                    flow.createHandler(context).start(container);
+
+                    Scene scene = new Scene( new JFXDecorator( stage , container.getView() ), 400, 300);
+                    scene.getStylesheets().add(getClass().getResource("/css/jfoenix-fonts.css").toExternalForm());
+                    scene.getStylesheets().add(getClass().getResource("/css/jfoenix-design.css").toExternalForm());
+                    scene.getStylesheets().add(getClass().getResource("/css/jfoenix-main-demo.css").toExternalForm());
+                    stage.setScene( scene );
+                    stage.show();
+
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
 
         submitButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
